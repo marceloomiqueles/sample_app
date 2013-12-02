456db3a0498c284bbb355ebf1e147228a82e4b20
Marcelo Miqueles <marcelomiqueles@hotmail.com>
2013-12-02 12:12:14 -0300

    Add a StaticPages controller

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '1eeb35316f6f4d975705c20ff9a7ea3d44aefd6cae0ca167d816b631c77f68cabc127f61f8c1e6d88ca00c95a4fb7e12180f5e47d838b4ddad0e18ba46dc3510'
+SampleApp::Application.config.secret_key_base = '0e5f1f141b697995e5545f11aece916bfa095238baa73aa6a60927b12365466f1b138c311a4cf4a709916bc361e85d8f2b75cb500f129c5d0b42648811d922b4'
